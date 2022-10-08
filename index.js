dd17fddb01ab77dffc00890e9d9d082459780b6c
prashant_handel <prashanthandel2501@gmail.com>
2022-10-08 15:07:58 +0530

    Project Done

---
+++ index.js
@@ -0,0 +1,25 @@
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  h >= 12 ? (ampm = "PM") : "AM";
+
+  h > 12 ? (h = h - 12) : h;
+
+  h = h < 10 ? "0" + h : h;
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl.innerText = ampm;
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+updateClock();
